1b7965c39ca3ff2e45e5979efb56494b6fcb0079
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-11 19:58:23 -0700

    O(n^2): bubble algorithm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import sort.Bubble;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bubble.bubbleAlgorithm();
     }
 }
