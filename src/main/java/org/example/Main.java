82518c5a4fd9cee56b3cbe7f5791bb2b6d04b8fd
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-20 13:52:25 -0700

    Recursion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,15 @@ package org.example;
 import sort.Bubble;
 import sort.Selection;
 
+import static sort.Recursion.iterativeFactorial;
+import static sort.Recursion.recursiveFactorial;
+
 public class Main {
     public static void main(String[] args) {
-//        Bubble.bubbleAlgorithm();
-        Selection.selectionAlgorithm();
+        int recursive = recursiveFactorial(3);
+        int iterative = iterativeFactorial(3);
+
+        System.out.println(recursive);
+        System.out.println(iterative);
     }
 }
