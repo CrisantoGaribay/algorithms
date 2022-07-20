125dcc4ea8c59e2b93c1dae8201954c89e4de781
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-20 15:56:17 -0700

    Merge sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,17 @@ package org.example;
 import sort.Bubble;
 import sort.Selection;
 
+import java.util.Arrays;
+
+import static sort.Merge.mergeSort;
 import static sort.Recursion.iterativeFactorial;
 import static sort.Recursion.recursiveFactorial;
 
 public class Main {
+    static int[] intArray = {20, 35, -15, 7, 55, 1, -22};
     public static void main(String[] args) {
-        int recursive = recursiveFactorial(3);
-        int iterative = iterativeFactorial(3);
+        mergeSort(intArray, 0, intArray.length);
 
-        System.out.println(recursive);
-        System.out.println(iterative);
+        System.out.println(Arrays.toString(intArray));
     }
 }
