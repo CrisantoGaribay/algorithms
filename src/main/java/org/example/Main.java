86c01a6df6083c9f7c322ad46c5f08013c17b722
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-20 16:46:06 -0700

    Merge sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,14 @@ import sort.Selection;
 import java.util.Arrays;
 
 import static sort.Merge.mergeSort;
+import static sort.QuickSort.quickSort;
 import static sort.Recursion.iterativeFactorial;
 import static sort.Recursion.recursiveFactorial;
 
 public class Main {
     static int[] intArray = {20, 35, -15, 7, 55, 1, -22};
     public static void main(String[] args) {
-        mergeSort(intArray, 0, intArray.length);
+        quickSort(intArray, 0, intArray.length);
 
         System.out.println(Arrays.toString(intArray));
     }
