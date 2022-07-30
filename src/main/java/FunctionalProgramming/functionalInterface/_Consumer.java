dc0aaefe14aa60b4cecc4f3f38bb2fdccd63e23a
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-30 12:51:49 -0700

    FP: bi consumer

--- src/main/java/FunctionalProgramming/functionalInterface/_Consumer.java
+++ src/main/java/FunctionalProgramming/functionalInterface/_Consumer.java
@@ -1,5 +1,6 @@
 package FunctionalProgramming.functionalInterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -7,15 +8,20 @@ public class _Consumer {
         Customer customer = new Customer("Maria", "9999999");
         greetCustomer(customer);
 
-        //Using the functional interface
+        //Using the consumer functional interface
         System.out.println("// using the consumer");
         greetCustomerConsumer.accept(customer);
+        greetCustomerConsumerv2.accept(customer, false);
 
     }
 
     static Consumer<Customer> greetCustomerConsumer = customer
             -> System.out.println("Hello " + customer.customerName +
             ", thanks for registering phone number " + customer.customerPhoneNumber);
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerv2 = (customer, showPhoneNumber)
+            -> System.out.println("Hello " + customer.customerName +
+            ", thanks for registering phone number " + (showPhoneNumber ? customer.customerPhoneNumber : "*******"));
 
     static void greetCustomer(Customer customer) {
         System.out.println("Hello " + customer.customerName +
