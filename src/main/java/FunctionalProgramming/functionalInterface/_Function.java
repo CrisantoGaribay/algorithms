829fd22167174f29b9c0c963d9d1afa202040c99
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-30 12:44:27 -0700

    FP: bi function

--- src/main/java/FunctionalProgramming/functionalInterface/_Function.java
+++ src/main/java/FunctionalProgramming/functionalInterface/_Function.java
@@ -1,9 +1,11 @@
 package FunctionalProgramming.functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
+        //Function takes 1 argument and produces 1 result
         int increment = increment(0);
         System.out.println("Method");
         System.out.println(increment);
@@ -17,6 +19,13 @@ public class _Function {
 
         System.out.println("Function chained");
         System.out.println(addBy1AndThenMultiplyBy10.apply(4));
+
+        //BiFunction takes 2 argument and produces 1 result
+        System.out.println(incrementByOneAndMultiply(4, 100));
+        System.out.println("BiFunction");
+        System.out.println(
+                incrementByOneAndMultiplyBiFunction.apply(4, 100)
+        );
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
@@ -25,5 +34,13 @@ public class _Function {
 
     static int increment(int number) {
         return number + 1;
+    }
+
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    static int incrementByOneAndMultiply(int number, int numberToMultiplyBy) {
+        return (number + 1) * numberToMultiplyBy;
     }
 }
