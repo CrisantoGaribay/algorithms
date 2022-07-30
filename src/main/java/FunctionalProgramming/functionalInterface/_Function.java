5ab0e12e974d558074dcf35214b535f6d37a2e56
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-30 11:55:46 -0700

    FP: Chaining methods

--- src/main/java/FunctionalProgramming/functionalInterface/_Function.java
+++ src/main/java/FunctionalProgramming/functionalInterface/_Function.java
@@ -11,9 +11,17 @@ public class _Function {
         Integer increment2 = incrementByOneFunction.apply(0);
         System.out.println("Function");
         System.out.println(increment2);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+
+        System.out.println("Function chained");
+        System.out.println(addBy1AndThenMultiplyBy10.apply(4));
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
 
     static int increment(int number) {
         return number + 1;
