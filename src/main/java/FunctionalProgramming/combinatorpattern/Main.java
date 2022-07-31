7ac7e9dc124bc128fef6ca5ea3e6f3343e94fc23
CrisantoGaribay <crisanto.garibay@unosquare.com>
2022-07-31 12:15:20 -0700

    FP: implementing combinator pattern

--- src/main/java/FunctionalProgramming/combinatorpattern/Main.java
+++ src/main/java/FunctionalProgramming/combinatorpattern/Main.java
@@ -2,15 +2,29 @@ package FunctionalProgramming.combinatorpattern;
 
 import java.time.LocalDate;
 
+import static FunctionalProgramming.combinatorpattern.CustomerRegistrationValidator.*;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(
                 "Alice",
                 "alice@mail.com",
-                "+03242323",
+                "+103242323",
                 LocalDate.of(2000, 1, 1)
         );
 
         System.out.println(CustomerValidatorService.isValid(customer));
+
+        // USING THE COMBINATOR PATTERN FOR FUNCTIONAL PROGRAMMING
+        ValidationResult result = isEmailValid()
+                .and(isPhoneValid())
+                .and(isAdult())
+                .apply(customer);
+        System.out.println("COMBINATOR");
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 }
